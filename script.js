84adf11d5d5ba408f2bdfa6d6a1e96f31bf133d2
rbuschettari <rodrigo_buschettari@hotmail.com>
2022-02-22 23:58:26 -0300

    Se agregan librerias

--- script.js
+++ script.js
@@ -135,6 +135,24 @@ efectoTarjetas.forEach(tarjeta => {
 
 });
 
+function alerta() {
+
+    Toastify({
+        text: "Agregado al carrito",
+        duration: 2000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function () { }
+    }).showToast();
+}
+
 //Inicia el programa
 
 cargarCarritoDelLocalStorage();
